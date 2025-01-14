ce8c9afed33b2b4e7ab3875490128f8343c331ab
Wiktor-Sikora <sikora.wiktor777@gmail.com>
2025-01-14 13:49:04 +0100

    db initialization on first run

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <fstream>
+#include <chrono>
+#include <thread>
+
 
 #include "utils.h"
+#include "db.h"
 
 time_t parseDateTime(const char* datetimeString, const char* format) {
     struct tm tmStruct;
@@ -16,3 +21,23 @@ std::string DateTime(time_t time, const char* format) {
     strftime(buffer, sizeof(buffer), format, timeinfo);
     return buffer;
 }
+
+void initilizeDbOnFirstRun() {
+    std::ifstream configFile("config.txt");
+    std::string fileContent{std::istreambuf_iterator<char>(configFile), std::istreambuf_iterator<char>()};
+
+    if(std::stoi(std::string{fileContent[20]}) == 1) {
+        configFile.close();
+        return;
+    }
+
+    configFile.close();
+    fileContent.erase();
+
+    std::ifstream sqlFile("src/db/scheme.sql");
+    std::string fileContent{std::istreambuf_iterator<char>(sqlFile), std::istreambuf_iterator<char>()};
+    std::cout << fileContent;    
+
+    std::chrono::seconds timespan(5);
+    std::this_thread::sleep_for(timespan);
+};
